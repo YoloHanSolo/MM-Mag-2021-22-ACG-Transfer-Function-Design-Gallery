041109b3c9accc1ad7ed4428b99a342d1dc932e3
JanPelicon <jp1467@student.uni-lj.si>
2022-05-14 04:29:27 +0200

    Custom model for TF generation

--- inspector.py
+++ inspector.py
@@ -1,34 +1,40 @@
+from cv2 import minEnclosingTriangle
 import numpy as np
 import cv2
-import math
+import matplotlib.pyplot as plt
 import random
+import json
+
+
+class RGBA:
+
+    def __init__(self, r, g, b, a) -> None:
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+        self.color = np.array([r,g,b,a], dtype=np.uint8)
+
+    def get(self):
+        return self.color
 
 class TransferFunctionGenerator:
 
-    def __init__(self, path, dtype=np.uint8, dimensions=None) -> None:
+    def __init__(self, path, dtype=np.uint8) -> None:
         self.path = path
         self.dtype = dtype
-        self.dimensions = dimensions
+
+        self.dimensions = None
         self.volume = None
-        self.transfer_function = None
+
+        self.bins = 256
+        self.histogram_raw = None
         self.histogram = None
         self.edges = None
-        self.features = None    
-        
-    def loadVolume(self):
-        print(self.path)
-        print("Volume Loading...")
-        volume = np.fromfile(self.path, dtype=self.dtype)
-        volume = np.reshape(volume, self.dimensions[::-1])
-        volume = np.moveaxis(volume, [0, 2], [2, 0])
-        self.mean_x = np.mean(volume, axis=0).astype(np.uint8)
-        self.mean_y = np.mean(volume, axis=1).astype(np.uint8)
-        self.mean_z = np.mean(volume, axis=2).astype(np.uint8)
-        self.max_x = np.amax(volume, axis=0).astype(np.uint8)
-        self.max_y = np.amax(volume, axis=1).astype(np.uint8)
-        self.max_z = np.amax(volume, axis=2).astype(np.uint8)
-        self.volume = volume
-        print("Volume Loaded!")
+
+        self.hue_tracker = None
+
+        self.transfer_function = None
 
     def setDimensions(self, dimensions=None):
         if dimensions:
@@ -36,101 +42,139 @@ class TransferFunctionGenerator:
         else:
             dimensions_string = self.path.split("_")[1]
             self.dimensions = tuple(map(int, dimensions_string.split("x")))
-        print(f"Dimensions: {self.dimensions[0]}x{self.dimensions[1]}x{self.dimensions[2]}")
 
-    def getRgbRandom(self):
-        return np.random.randint(0, 255, (3), dtype=np.uint8)
+    def loadVolume(self):
+        volume = np.fromfile(self.path, dtype=self.dtype)
+        volume = np.reshape(volume, self.dimensions[::-1])
+        volume = np.moveaxis(volume, [0, 2], [2, 0])
+        self.volume = volume
 
-    def getRgbRange(self, min_val=0, max_val=255):
-        if max_val == 0:
-            max_val = 1
-        if min_val >= max_val:
-            min_val = max_val-1
-        return np.random.randint(
-            max(min_val, 0),
-            min(max_val, 255),
-            (3), dtype=np.uint8)
+    def getVolumeStatisticalProperties(self, display=False):
+        self.mean_x = np.mean(self.volume, axis=0).astype(np.uint8)
+        self.mean_y = np.mean(self.volume, axis=1).astype(np.uint8)
+        self.mean_z = np.mean(self.volume, axis=2).astype(np.uint8)
+        self.max_x = np.amax(self.volume, axis=0).astype(np.uint8)
+        self.max_y = np.amax(self.volume, axis=1).astype(np.uint8)
+        self.max_z = np.amax(self.volume, axis=2).astype(np.uint8)
+        self.min_x = np.amin(self.volume, axis=0).astype(np.uint8)
+        self.min_y = np.amin(self.volume, axis=1).astype(np.uint8)
+        self.min_z = np.amin(self.volume, axis=2).astype(np.uint8)
+        if display:
+            img_mean = np.concatenate((self.mean_x, self.mean_y), axis=1)
+            img_mean = np.concatenate((img_mean, self.mean_z), axis=1)
+            img_max = np.concatenate((self.max_x, self.max_y), axis=1)
+            img_max = np.concatenate((img_max, self.max_z), axis=1)
+            img_min = np.concatenate((self.min_x, self.min_y), axis=1)
+            img_min = np.concatenate((img_min, self.min_z), axis=1)
+            cv2.imshow("mean", img_mean)
+            cv2.imshow("max", img_max)
+            cv2.imshow("min", img_min)
+            cv2.waitKey(0)
 
-    def getHSVColor(self, h, s, v):
-        return cv2.cvtColor(np.array([[[h, s, v]]], dtype=np.uint8), cv2.COLOR_HSV2BGR).flatten()
-
-    """
-    render
-    """
-
-    def renderZ(self):
-        for z in range(self.dimensions[2]):    
-            layer_z = self.volume[:, :, z]      
-            image = np.ndarray((self.dimensions[0], self.dimensions[1], 3), dtype=np.uint8)
-            for ix in range(self.dimensions[0]):
-                for iy in range(self.dimensions[1]):
-                    pixel = layer_z[ix, iy]
-                    image[ix, iy, :] = self.transfer_function[pixel]     
-
-            cv2.imshow("z-layer", cv2.resize(image, (self.dimensions[0], self.dimensions[1])))
-            cv2.waitKey(1)
-
-    def renderX(self):
-        for x in range(self.dimensions[0]):    
-            layer_x = self.volume[x, :, :]      
-            image = np.ndarray((self.dimensions[1], self.dimensions[2], 3), dtype=np.uint8)
-            for iy in range(self.dimensions[1]):
-                for iz in range(self.dimensions[2]):
-                    pixel = layer_x[iy, iz]
-                    image[iy, iz, :] = self.transfer_function[pixel]                          
-            cv2.imshow("x-layer", cv2.resize(image, (512, 512)))
-            cv2.waitKey(1)
-
-    def renderMean(self):
-        cv2.imshow("mean_x", cv2.resize(self.mean_x, (self.dimensions[0], self.dimensions[1])))
-        cv2.imshow("mean_y", cv2.resize(self.mean_y, (self.dimensions[0], self.dimensions[1])))
-        cv2.imshow("mean_z", cv2.resize(self.mean_z, (self.dimensions[0], self.dimensions[1])))
-        cv2.waitKey(0)
+    def HSV2RGB(self, hue, saturation, value):
+        hsv = np.array([[[hue, saturation, value]]], dtype=np.uint8)
+        rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR).flatten()
+        return rgb
 
     """
     TF
     """
-    
-    def generateRangeTF(self):
-        transfer_function = {}
-        for intensity in range(0, 256, 32):
-            color = self.getRgbRange(intensity, intensity + 32)
-            for each in range(intensity, intensity + 32):
-                transfer_function[each] = color 
-        return transfer_function   
 
-    def generateHistogramTF(self, bins):
-        print("Generating hist TF...")
-
-        #histogram = np.bincount(self.volume.flatten()) # calculate histogram
+    def getVolumeHistogramRaw(self): # USELESS FIX TODO
         histogram, edges = np.histogram(self.volume, bins=256)
+        self.histogram_raw = histogram
+
+    def getVolumeHistogramProcessed(self, bins, display=False):       
+        histogram, edges = np.histogram(self.volume, bins=bins) # FIX TODO
+        # BG noise cut
+        cutoff_limit = np.average(histogram) / 2
+        for bin in range(bins-1, 0, -1):
+            dy = histogram[bin-1] - histogram[bin]
+            if dy > cutoff_limit: 
+                histogram[bin-1] = 0
         histogram = np.log(histogram + 1)
         histogram = histogram / np.max(histogram)
-        histogram = (histogram * 255).astype(np.uint8) # cast to uint8
+        histogram = (histogram * 255).astype(np.uint8)
+        self.histogram = histogram
+        self.edges = edges
+        self.bins = bins
+        if display:
+            plt.plot(
+                list(range(0, bins)),
+                histogram, 'o')
+            plt.show()
 
-        if (histogram[0] == 255):
-            histogram[0] = 0
+    def getSaturation(self):
+        x = random.uniform(0, 1)
+        y = (1 / (2*x + 1)) * 255 
+        return 255
+        #return max(min(255, y), 0)
 
-        #import matplotlib.pyplot as plt
-        #x = list(range(0, 256))
-        #plt.plot(x, histogram, 'o')
-        #plt.show()
+    def getHue(self):
+        scaling = 256 // self.bins
+        offset = random.randint(scaling, scaling)
+        self.hue_tracker = (self.hue_tracker + offset) % 256 
+        return self.hue_tracker
 
-        print(histogram)
+    def getValue(self, value):
+        return max((((value / 255) - 1) ** 4) * 255, 100)
 
+    def getOpacity(self, value):
+        return min(((value / 255) ** 4) / 1.5, 0.3) * 255
+
+    def get1DTransferFunction(self, display=False):
         transfer_function = {}
-        for bin_index, bin_value in enumerate(histogram):
-            color = self.getHSVColor(bin_index, bin_value, bin_value)
-            transfer_function[bin_index] = color
-
+        self.hue_tracker = random.randint(0, 255)
+        for bin_index, bin_value in enumerate(self.histogram):
+            border_min = round(self.edges[bin_index])
+            border_max = round(self.edges[bin_index+1]) + 1
+            color = self.HSV2RGB(
+                self.getHue(), 
+                self.getSaturation(), 
+                self.getValue(border_max))
+            opacity = 0
+            if random.uniform(0, 1) > min(0.02 * self.bins, 0.7): # DROPOUT
+                opacity = self.getOpacity(border_min)
+            rgba = [color[2], color[1], color[0], opacity]
+            for pixel_val in range(border_min, border_max):
+                transfer_function[pixel_val] = rgba
         self.transfer_function = transfer_function
 
-    def generateRandomTF(self):
-        transfer_function = {}
-        for intensity in range(0, 256):
-            transfer_function[intensity] = self.getRgbRandom()
-        return transfer_function    
+        if display:
+            TF_2D = np.ndarray((256,256,4), dtype=np.uint8)
+            for pixel_val in range(256):
+                TF_2D[:, pixel_val, 0] = self.transfer_function[pixel_val][2]
+                TF_2D[:, pixel_val, 1] = self.transfer_function[pixel_val][1]
+                TF_2D[:, pixel_val, 2] = self.transfer_function[pixel_val][0]
+                TF_2D[:, pixel_val, 3] = self.transfer_function[pixel_val][3]
+            cv2.imshow("TF_2D_rgb", TF_2D[:, :, 0:3])
+            cv2.imshow("TF_2D_alpha", TF_2D[:, :, 3])
+            cv2.waitKey(1)
 
+    def _vptBumpForm(self, index, color):
+        form = {
+            "position": { "x": index / 256, "y": index / 256 },
+            "size": { "x": 1 / 256, "y": 1 },
+            "color": {
+                "r": color[0] / 255,
+                "g": color[1] / 255,
+                "b": color[2] / 255,
+                "a": color[3] / 255 }}
+        return form
+
+    def saveTransferFunctionVPT(self, name):
+        TF_json = []
+        prev_val = None
+        for pixel_val in range(256):
+            TF_json.append(
+                self._vptBumpForm(pixel_val, self.transfer_function[pixel_val]))
+
+            prev_val = self.transfer_function[pixel_val]
+
+        with open(name + '.json', 'w') as file:
+            json.dump(TF_json, file)
+
+    """
     def generatePreview(self):
         image = np.ndarray((self.dimensions[0], self.dimensions[1], 3), dtype=np.uint8)
         for iy in range(self.dimensions[0]):
@@ -146,32 +190,41 @@ class TransferFunctionGenerator:
                 image[iy, iz, :] = self.transfer_function[pixel]                          
         cv2.imshow("preview_mz", image)
         cv2.waitKey(1)
+    """
 
-        image = np.ndarray((self.dimensions[0], self.dimensions[1], 3), dtype=np.uint8)
-        for iy in range(self.dimensions[0]):
-            for iz in range(self.dimensions[1]):
-                pixel = (self.max_z[iy, iz] + self.mean_z[iy, iz]) // 2
-                image[iy, iz, :] = self.transfer_function[pixel]                          
-        cv2.imshow("preview_gz", image)
-        cv2.waitKey(1)
+    """
+    render
+    """
+
+    def renderZ(self):
+        for z in range(self.dimensions[2]):    
+            layer_z = self.volume[:, :, z]      
+            image = np.ndarray((self.dimensions[0], self.dimensions[1], 3), dtype=np.uint8)
+            for ix in range(self.dimensions[0]):
+                for iy in range(self.dimensions[1]):
+                    pixel = layer_z[ix, iy]
+                    image[ix, iy, :] = self.transfer_function[pixel][0:3]     
+            cv2.imshow("z-layer", cv2.resize(image, (self.dimensions[0], self.dimensions[1])))
+            cv2.waitKey(1)
 
 #filename = "raw/body_512x512x226_1x1x1_uint8.raw"
-#filename = "raw/engine_256x256x256_1x1x1_uint8.raw"
+filename = "raw/engine_256x256x256_1x1x1_uint8.raw"
 #filename = "raw/mri_ventricles_256x256x124_1x1x1_uint8.raw"
-filename = "raw/brain_512x512x230_5x5x9_uint8.raw"
-
-
-
+#filename = "raw/brain_512x512x230_5x5x9_uint8.raw"
 
 TFG = TransferFunctionGenerator(filename)
-#TFG.setDimensions((256,256,124))
-TFG.setDimensions()
+TFG.setDimensions() #TFG.setDimensions((256,256,124))
 TFG.loadVolume()
-#TFG.renderMean()
-#TFG.generateRandomTF()
-TFG.generateHistogramTF(256)
-TFG.generatePreview()
-TFG.renderZ()
-#TFG.renderX()  
+TFG.getVolumeStatisticalProperties(display=False)
+TFG.getVolumeHistogramRaw()
+
+bins = 3
+for tf in range(9):
+    TFG.getVolumeHistogramProcessed(bins=bins, display=False)
+    TFG.get1DTransferFunction(display=False)
+    TFG.saveTransferFunctionVPT(name="transfer_function_{}".format(tf))
+    bins = bins + 2
+#TFG.generatePreview()
+#TFG.renderZ()
 
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -6,31 +6,104 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+request_id = 0
 
-@app.route("/")
-def index():
-  return redirect("/transfer-function", code=302)
-
-@app.route("/transfer-function")
-@cross_origin()
-def getTransferFunctions():
-    data = {"transfer_functions": [
-        {"name": "tf1", "value": "tf1-new"}, 
-        {"name": "tf2", "value": "tf2-new"},
-        {"name": "tf3", "value": "tf3-new"}, 
-        {"name": "tf4", "value": "tf4-new"},
-        {"name": "tf5", "value": "tf5-new"}, 
-        {"name": "tf6", "value": "tf6-new"},
-        {"name": "tf7", "value": "tf7-new"}, 
-        {"name": "tf8", "value": "tf8-new"},
-        {"name": "tf9", "value": "tf9-new"}]}
-
+def responseHandler(data):
     response = jsonify(data)
-    response.status_code = 200
+    response.statusCode = 200
     return response
 
+@app.route("/", methods = ['GET'])
+def getIndex():
 
+    data = {
+        "author": "Jan Pelicon",
+        "date": "13.05.2022",
+        "API-description": "A design gallery for transfer functions.\
+        Handles volume feature vectors in order to generate a set of good transfer functions from which user picks one and optionaly epxlores further",
+        "routes": [
+            {
+                "name": "/",
+                "method": "GET",
+                "description": "Index page and information about API."
+            },
+            {
+                "name": "/generate-tf",
+                "method": "POST",
+                "description": "user posts volume feature vector - server generates 9 transfer functions"
+            },
+            {
+                "name": "/explore-tf",
+                "method": "POST",
+                "description": "user posts transfer function - server generates new transfer functions that are similar to the recieved one"
+            }     
+        ]}
 
+    return responseHandler(data)
+
+@app.route("/generate-tf", methods = ['POST'])
+@cross_origin()
+def postGenerateTF():
+
+    print(request.data)
+
+    data = {
+        "transfer_functions": [
+            {"name": "tf1", "value": "feature-tf"}, 
+            {"name": "tf2", "value": "feature-tf"},
+            {"name": "tf3", "value": "feature-tf"}, 
+            {"name": "tf4", "value": "feature-tf"},
+            {"name": "tf5", "value": "feature-tf"}, 
+            {"name": "tf6", "value": "feature-tf"},
+            {"name": "tf7", "value": "feature-tf"}, 
+            {"name": "tf8", "value": "feature-tf"},
+            {"name": "tf9", "value": "feature-tf"}
+        ]
+    }
+
+    return responseHandler(data)
+
+@app.route("/explore-tf", methods = ['POST'])
+@cross_origin()
+def postExploreTF():
+
+    print(request.data)
+
+    data = {"transfer_functions": [
+        {"name": "tf1", "value": "explore-tf"}, 
+        {"name": "tf2", "value": "explore-tf"},
+        {"name": "tf3", "value": "explore-tf"}, 
+        {"name": "tf4", "value": "explore-tf"},
+        {"name": "tf5", "value": "explore-tf"}, 
+        {"name": "tf6", "value": "explore-tf"},
+        {"name": "tf7", "value": "explore-tf"}, 
+        {"name": "tf8", "value": "explore-tf"},
+        {"name": "tf9", "value": "explore-tf"}]
+        }
+
+    return responseHandler(data)
+
+@app.route("/generate-random-tf", methods = ['GET'])
+@cross_origin()
+def postGenerateRandomTF():
+
+    print(request.data)
+
+    data = {
+        "transfer_functions": [
+            {"name": "tf1", "value": "random-tf"}, 
+            {"name": "tf2", "value": "random-tf"},
+            {"name": "tf3", "value": "random-tf"}, 
+            {"name": "tf4", "value": "random-tf"},
+            {"name": "tf5", "value": "random-tf"}, 
+            {"name": "tf6", "value": "random-tf"},
+            {"name": "tf7", "value": "random-tf"}, 
+            {"name": "tf8", "value": "random-tf"},
+            {"name": "tf9", "value": "random-tf"}
+        ]
+    }
+
+    return responseHandler(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
